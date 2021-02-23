1143a19a066a4409c9a120d8440602854f962611
Anna <anna.mcphee@gmail.com>
2021-02-24 08:34:57 +1300

    Add trait firstname

--- Main.java
+++ Main.java
@@ -18,6 +18,7 @@ public class Main {
 class Person {
     // todo: Include at least four other 'traits' that a Person might have that usually differ from other People.
     private String surname;
+    private String firstname;
 
 
     // todo: Update this constructor to include the additional traits you made.
